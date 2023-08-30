6621d8a7ed5f60ba6f3572e05da4b02b8360d2d6
K Harsh <harshnaidu2u@gmail.com>
2023-08-30 14:45:11 +0530

    comments added --> how code works

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const API_KEY = "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";
+// API_KEY used one
+// to use image generator add new api key
+
 const submitInput = document.querySelector("#submit-section");
 const inputElement = document.querySelector("input");
 const imageSection = document.querySelector(".image-section");
@@ -6,31 +9,50 @@ const imageSection = document.querySelector(".image-section");
 const getImages = async() => {
     const options = {
         method: "POST",
+        // posting the data using POST
+
         headers: {
             "Authorization": `Bearer ${API_KEY}`,
             "Content-Type": "application/json"
+            // openAI method
         },
         body: JSON.stringify({
              prompt: inputElement.value,
+             // input value is accessed
+
              n: 4,
+             // number of images
+
              size: "1024x1024"
+             // methods of costing the images 
         })
     }
     try {
         const response = await fetch('https://api.openai.com/v1/images/generations', options);
+        // image generation link 
+
         const data = await response.json();
         console.log(data);
+        //data fetched by the link
 
         data?.data.forEach(element => {
             const imageContainer = document.createElement("div");
+            // creating image contianer for each image (4)
+
             imageContainer.classList.add("image-container");
+            // adding classList image-container with css properties
 
             const imageElement = document.createElement('img')
+            // display image (4)
+
             imageElement.setAttribute('src', element.url)
+            // setting attribute src to url in data in console.log(data)
 
             imageContainer.append(imageElement);
+            // appending images in image-container
 
             imageSection.append(imageContainer)
+            //appending image-container in image-section
         });
     } catch (error) {
         console.log(error);
@@ -38,3 +60,4 @@ const getImages = async() => {
 }
 
 submitInput.addEventListener('click', getImages)
+// click event to fetch the data form url
